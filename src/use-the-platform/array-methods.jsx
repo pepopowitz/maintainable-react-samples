9d80d1c918c45bcb59d11f82254fba1c34656539
Steven Hicks <steven.j.hicks@gmail.com>
2018-03-09 10:01:14 -0600

    array-methods kittens

--- src/use-the-platform/array-methods.jsx
+++ src/use-the-platform/array-methods.jsx
@@ -2,3 +2,46 @@ import React from "react";
 export default function() {
   return <div>array methods</div>;
 }
+
+const kittens = [
+  {
+    name: 'Turtle',
+    color: 'gray',
+    lovesMe: false,
+  },
+  {
+    name: 'Flower',
+    color: 'black',
+    lovesMe: true,
+    dead: true,
+  },
+  {
+    name: 'Potatoes',
+    color: 'gray',
+    lovesMe: true,
+    dead: true,
+  }
+]
+
+let myKittens = [];
+for (let i = 0; i < kittens.length; i++) {
+  const kitten = kittens[i];
+  if (kitten.color === 'gray') {
+    myKittens.push(kitten.name);
+  }
+}
+let myFavoriteKitten;
+for (let i = 0; i < myKittens.length; i++) {
+  const kitten = myKittens[i];
+  if (kitten === 'Potatoes') {
+    myFavoriteKitten = kitten;
+  }
+}
+console.log(myFavoriteKitten);
+
+const myFavoriteKitten2 = kittens
+  .filter(kitten => kitten.color === 'gray')
+  .map(kitten => kitten.name)
+  .find(kitten => kitten === 'Potatoes');
+
+console.log(myFavoriteKitten2);
